b715d02f845b660435f1f31b0c7479ca2e1e5da9
XSilverTH <XSilverTH@outlook.com>
2022-11-18 16:39:03 +0330

    Add project files.

---
+++ XUtils/FileManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace XUtils;
+
+public class FileManager
+{
+    public static void Save(object thing, string fileName)
+    {
+        File.WriteAllText(fileName, JsonSerializer.Serialize(thing));
+    }
+
+    public static T? Load<T>(string fileName)
+    {
+        try
+        {
+            return JsonSerializer.Deserialize<T>(File.ReadAllText(fileName));
+        }
+        catch (Exception)
+        {
+            return default;
+        }
+    }
+}
---
+++ XUtils/Print.cs
@@ -0,0 +1,81 @@
+﻿namespace XUtils;
+
+public class Print
+{
+    public static void Header(string title)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Clear();
+        Console.WriteLine($"{title} Page\n{DateTime.Now.ToString("g")}\n{new string('*', 20)}");
+        Console.ResetColor();
+    }
+
+    public static short Menu(string text, string[] options, bool? hasBack)
+    {
+        #region Printing the menu
+
+        Console.Write(text == "d" ? "What do you want to do?" : text);
+        int i = 1;
+        if (options.Length <= 8)
+        {
+            foreach (string option in options)
+                Console.Write($"{(i == 1 ? " (" : ", ")}{i++}.{option}");
+            if (hasBack == true)
+                Console.WriteLine($", {i}.Back)");
+            else if (hasBack == false)
+                Console.WriteLine($", {i}.Exit)");
+            else
+                Console.WriteLine(")");
+        }
+        else
+        {
+            Console.WriteLine();
+            foreach (string option in options)
+                Console.WriteLine($"{i++}.{option}");
+            if (hasBack == true)
+                Console.WriteLine(i + ".Back");
+            else if (hasBack == false)
+                Console.WriteLine(i + ".Exit");
+        }
+
+        #endregion Printing the menu
+
+        #region Getting an input
+
+        short q;
+        while (true)
+        {
+            q = 1;
+            string input = Console.ReadLine().ToLower();
+            short inputS;
+            if (short.TryParse(input, out inputS) &&
+                ((inputS > 0 && inputS <= options.Length + 1) || (hasBack != null && inputS == i)))
+                return inputS;
+            if ((hasBack == true && input == "back") || (hasBack == false && input == "exit"))
+                return (short)i;
+            foreach (string option in options)
+            {
+                if (input == option.ToLower())
+                    return q;
+
+                q++;
+            }
+
+            Console.WriteLine("Invalid input");
+        }
+
+        #endregion Getting an input
+    }
+
+    public static void WDelay(string text, int delay)
+    {
+        string[] parts = text.Split('^');
+        foreach (string part in parts)
+        {
+            Console.Write(part);
+            Thread.Sleep(delay);
+        }
+
+        Console.WriteLine();
+    }
+}
