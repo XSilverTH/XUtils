9e6bbfdf58a52ab8adb9ec8c6331c9e98a82c663
XSilverTH <XSilverTH@outlook.com>
2023-12-19 20:31:35 +0330

    Reformat and cleanup

--- XUtils/Print.cs
+++ XUtils/Print.cs
@@ -15,10 +15,10 @@ public class Print
         #region Printing the menu
 
         Console.Write(text == "d" ? "What do you want to do?" : text);
-        int i = 1;
+        var i = 1;
         if (options.Length <= 8)
         {
-            foreach (string option in options)
+            foreach (var option in options)
                 Console.Write($"{(i == 1 ? " (" : ", ")}{i++}.{option}");
             if (hasBack == true)
                 Console.WriteLine($", {i}.Back)");
@@ -30,7 +30,7 @@ public class Print
         else
         {
             Console.WriteLine();
-            foreach (string option in options)
+            foreach (var option in options)
                 Console.WriteLine($"{i++}.{option}");
             if (hasBack == true)
                 Console.WriteLine(i + ".Back");
@@ -46,14 +46,14 @@ public class Print
         while (true)
         {
             q = 1;
-            string input = Console.ReadLine().ToLower();
+            var input = Console.ReadLine().ToLower();
             int inputS;
             if (int.TryParse(input, out inputS) &&
                 ((inputS > 0 && inputS <= options.Length + 1) || (hasBack != null && inputS == i)))
                 return inputS;
             if ((hasBack == true && input == "back") || (hasBack == false && input == "exit"))
                 return i;
-            foreach (string option in options)
+            foreach (var option in options)
             {
                 if (input == option.ToLower())
                     return q;
@@ -69,8 +69,8 @@ public class Print
 
     public static void WDelay(string text, int delay)
     {
-        string[] parts = text.Split('^');
-        foreach (string part in parts)
+        var parts = text.Split('^');
+        foreach (var part in parts)
         {
             Console.Write(part);
             Thread.Sleep(delay);
@@ -81,7 +81,7 @@ public class Print
 
     public static string Input(string text, bool nl = true)
     {
-        Console.Write(text + (nl?"\n":""));
+        Console.Write(text + (nl ? "\n" : ""));
         return Console.ReadLine();
     }
 }
