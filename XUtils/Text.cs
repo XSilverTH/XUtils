3f496efcdd6cee536fc4326ca49bf1de9e33b03c
XSilverTH <XSilverTH@outlook.com>
2023-12-19 21:05:01 +0330

    Added TextToArgs feature

---
+++ XUtils/Text.cs
@@ -0,0 +1,19 @@
+﻿namespace XUtils;
+
+public class Text
+{
+    public static string[] ToArgs(string input)
+    {
+        var parmChars = input.ToCharArray();
+        var inQuote = false;
+        for (var index = 0; index < parmChars.Length; index++)
+        {
+            if (parmChars[index] == '"')
+                inQuote = !inQuote;
+            if (!inQuote && parmChars[index] == ' ')
+                parmChars[index] = '\n';
+        }
+
+        return new string(parmChars).Split('\n', StringSplitOptions.RemoveEmptyEntries);
+    }
+}
